f0e162b2b964c375e6e7a688db41499706c8cc43
Symone Seldon <seldon_one8@yahoo.com>
2021-11-14 00:43:08 -0500

    prompt for more

--- app.js
+++ app.js
@@ -5,17 +5,39 @@ return inquirer.prompt([
         {
           type: 'input',
           name: 'name',
-          message: 'What is your name?'
+          message: 'What is your name? (Required)',
+          validate: nameInput => {
+              if (nameInput) {
+                  return true;
+              } else {
+                  console.log('Please enter your name!');
+                  return false;
+              }
+          }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username',
+            validate: gitInput => {
+                if (gitInput) {
+                    return true;
+                } else {
+                    console.log('Please enter github username!');
+                }
+            }
         },
         {
             type:'input',
             name:'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };  
@@ -26,16 +48,34 @@ return inquirer.prompt([
       console.log(`
       add a new Project
       `);
-      return inquirer.prompt([
+      if (!portfolioData.projects) {
+          portfolioData.projects = [];
+      }
+      return inquirer
+      .prompt([
           {
               type: 'input',
               name: 'name',
-              message: 'What is the name of your Project'
+              message: 'What is the name of your Project',
+              validate: projectName => {
+                  if (projectName) {
+                      return true;
+                  } else {
+                      console.log('Please enter project name!');
+                  }
+              }
           },
           {
               type: 'input',
               name: 'description',
-              message: 'Provide a description of the project (Required)'
+              message: 'Provide a description of the project (Required)',
+              validate: projectInfo => {
+                  if (projectInfo) {
+                      return true;
+                  } else {
+                      console.log('Plase enter description!');
+                  }
+              }
           },
           {
               type: 'checkbox',
@@ -46,7 +86,14 @@ return inquirer.prompt([
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: gitLink => {
+                if (gitLink) {
+                    return true;
+                } else {
+                    console.log('Please provide link');
+                }
+            }
           },
           {
             type: 'confirm',
